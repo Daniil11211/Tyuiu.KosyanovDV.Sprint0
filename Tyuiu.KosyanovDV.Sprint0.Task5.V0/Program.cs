8982d8b9ea4700699b647c6a000e0af3ac2ed178
Daniil Kosyanov <kosyanov.daniil@inbox.ru>
2024-09-13 21:15:54 +0300

    Task5

---
+++ Tyuiu.KosyanovDV.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KosyanovDV.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.KosyanovDV.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(8, 9));
+            Console.WriteLine("A + B = " + DataService.Substraction(10, 9));
+            Console.WriteLine("A + B = " + DataService.Multiplication(8, 9));
+            Console.WriteLine("A + B = " + DataService.Division(36, 9));
+            Console.ReadKey();
+        }
+    }
+}
